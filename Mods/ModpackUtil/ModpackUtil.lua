09d9e07dc834c2aa22e6cb1b5d641716e85f0361
Dimserene <raul0520@gmail.com>
2024-07-21 00:18:12 +0800

    updates

--- Mods/ModpackUtil/ModpackUtil.lua
+++ Mods/ModpackUtil/ModpackUtil.lua
@@ -3,8 +3,19 @@
 --- MOD_ID: Modpack_Util
 --- MOD_AUTHOR: [Dimserene]
 --- MOD_DESCRIPTION: Dimserene's Modpack Utility
+--- VERSION: Fine-tuned
+--- PRIORITY: -999999999999999999999999
 ----------------------------------------------
 ------------MOD CODE -------------------------
+
+if SMODS.Atlas then
+  SMODS.Atlas({
+    key = "modicon",
+    path = "icon.png",
+    px = 32,
+    py = 32
+  })
+end
 
 local MODPACK_VERSION = "Dimserene's Modpack - Fine-tuned"
 
