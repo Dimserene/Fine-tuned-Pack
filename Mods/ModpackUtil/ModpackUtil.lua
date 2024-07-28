229f5c0ab859c1ace11fef58001f57cdc81899fd
Dimserene <raul0520@gmail.com>
2024-07-28 23:41:39 +0800

    2024/07/28 15:41:31

--- Mods/ModpackUtil/ModpackUtil.lua
+++ Mods/ModpackUtil/ModpackUtil.lua
@@ -8,6 +8,8 @@
 ----------------------------------------------
 ------------MOD CODE -------------------------
 
+local nativefs = require("nativefs")
+
 if SMODS.Atlas then
   SMODS.Atlas({
     key = "modicon",
@@ -17,7 +19,15 @@ if SMODS.Atlas then
   })
 end
 
-local MODPACK_VERSION = "Dimserene's Modpack - Fine-tuned"
+
+-- read version.txt
+local version = nativefs.read(lovely.mod_dir .. "/ModpackUtil/version.txt")
+
+local updated = os.date("!%Y/%m/%d %H:%M:%S", love.filesystem.getLastModified(lovely.mod_dir .. "/ModpackUtil/version.txt"))
+
+
+local MODPACK_VERSION = "Dimserene's Modpack - Fine-tuned" .. "\nCurrent Version: " .. version .. "     Last Update: " .. updated
+
 
 local gameMainMenuRef = Game.main_menu
 function Game:main_menu(change_context)
